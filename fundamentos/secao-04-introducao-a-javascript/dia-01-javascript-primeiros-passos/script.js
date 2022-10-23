4057d74eb940d890c3c7ffd430ac3f66bf5daf8c
rnogara <r.nogara.dev@gmail.com>
2022-10-23 18:07:36 -0300

    adiciona os dois primeiros exercícioas da aula

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,26 @@
+//Calculadora
+const a = 3;
+const b = 7;
+
+const adicao = a + b;
+const subtracao = a - b;
+const multiplicacao = a * b;
+const divisao = a / b;
+const modulo = a % b;
+
+//Comparando o maior de dois números
+ const c = 5;
+ const d = 20;
+
+ if (c > d){
+    console.log( c + 'é o maior número');
+ } else if (d > c){
+    console.log( d + 'é o maior número');
+ } else if ( c === d ){
+    console.log('os dois números são iguais');
+ } else {
+    console.log('ops... algo deu errado');
+ }
+
+ //Comparando o maior de três números
+ 
