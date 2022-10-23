db856f12a0abae99fefc09c7950d727327a354c0
rnogara <r.nogara.dev@gmail.com>
2022-10-23 18:17:40 -0300

    adiciona adiciona os exercicios 3 e 4 da aula

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -23,4 +23,31 @@ const modulo = a % b;
  }
 
  //Comparando o maior de três números
- 
+ const e = 4;
+ const f = 12;
+ const g = 3;
+
+ if (e > f && e > g){
+    console.log( e + 'é o maior número');
+ } else if (f > e && f > g){
+    console.log( f + 'é o maior número');
+ } else if (g > e && g > f){
+    console.log( g + 'é o maior número');
+ } else if (e === f && f === g){
+    console.log('os três números são iguais');
+ } else {
+    console.log('ops... walgo deu errado');
+ }
+
+ //Positivo ou Negativo?
+ const h = 4;
+
+ if (h > 0){
+    console.log('Esse número é positivo');
+ } else if (h < 0){
+    console.log('Esse número é negativo');
+ } else if (h === 0){
+    console.log('Esse número é zero');
+ } else {
+    console.log('ops... algo deu errado');
+ }
