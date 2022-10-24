67da944a17f6d9aeacc26f887d74b37be055fd3e
rnogara <r.nogara.dev@gmail.com>
2022-10-24 12:40:20 -0300

    adiciona parte2 do exercicio1

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -1,10 +1,47 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 var soma = 0;
+var maiorNumero = 0;
+var quantidadeImpares = 0;
+var mensagemSemImpar = '';
+var menorNumero = Infinity;
 
+console.log('Estes são os numeros dentro do array:');
 for (let i = 0; i < numbers.length; i += 1){
   console.log(numbers[i]);
 
   soma += numbers[i];
+
+  if (numbers[i] > maiorNumero) {
+    maiorNumero = numbers[i];
+  }
+
+  if (numbers[i] % 2 !== 0){
+    quantidadeImpares += 1;
+  } else if (quantidadeImpares === 0){
+    mensagemSemImpar = 'Nenhum valor ímpar encontrado';
+  }
+
+  if (numbers[i] < menorNumero) {
+    menorNumero = numbers[i];
+  }
 }
 
-console.log(soma);
+console.log('');
+console.log('Esta é a soma dos numeros dentro do array: ' + soma);
+
+var media = soma / (numbers.length - 1);
+console.log ('Esta é a média dos numeros dentro do array: ' + media.toFixed(0));
+
+if (media > 20){
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor ou igual a 20');
+}
+
+console.log('');
+console.log('O maior numero dentro do array é: ' + maiorNumero);
+
+console.log('Existem ' + quantidadeImpares + ' números ímpares dentro do array');
+console.log(mensagemSemImpar);
+
+console.log('O menor numero dentro do array é: ' + menorNumero);
