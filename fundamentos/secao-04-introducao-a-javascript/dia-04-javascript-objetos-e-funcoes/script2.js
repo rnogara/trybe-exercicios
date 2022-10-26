359e0e95e270ef8a9b9b068494f649c4de89d07e
rnogara <r.nogara.dev@gmail.com>
2022-10-26 18:28:58 -0300

    adiciona parte 1 exercicios sobre objetos e for/in

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/script2.js
@@ -0,0 +1,12 @@
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
