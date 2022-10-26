6b40364ed5f5ba8f854fae89113af4d7a2930805
rnogara <r.nogara.dev@gmail.com>
2022-10-26 18:37:39 -0300

    adiciona parte de funcoes dos exercicios do dia

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/script2.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/script2.js
@@ -10,3 +10,14 @@ let leitor = {
     },
   ],
 };
+
+console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} é ${leitor.livrosFavoritos[0].titulo}`);
+
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+});
+
+console.log('');
+console.log(`${leitor.nome} tem ${(leitor.livrosFavoritos.length)} livros favoritos`);
