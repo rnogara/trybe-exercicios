f9e5c95c7c2e61ca17d45259f3833cde4af368b4
rnogara <r.nogara.dev@gmail.com>
2022-11-22 22:18:03 -0300

    adiciona ex3wwwwwwwwwww

---
+++ fundamentos/secao-07-Introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-código/exercicio04.test.js
@@ -0,0 +1,6 @@
+const { printMessage } = require('./exercicio04');
+
+// Crie uma função de teste para validar se a mensagem de erro é lançada caso a função seja chamada com um objeto inválido.
+it('Se não pasar parâmetro de objeto na função printMessage, deve retornar um erro', () => {
+  
+});
