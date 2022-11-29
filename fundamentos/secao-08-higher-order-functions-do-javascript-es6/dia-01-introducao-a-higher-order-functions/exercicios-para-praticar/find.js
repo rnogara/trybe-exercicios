03d7a8a09733892e9cdd1f0f6daa0c34d99ffceb
rnogara <r.nogara.dev@gmail.com>
2022-11-29 15:35:27 -0300

    adiciona pratica find

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicios-para-praticar/find.js
@@ -0,0 +1,31 @@
+// Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// Adicione seu código aqui
+
+numbers.find((n) => n % 3 === 0 && n % 5 === 0);
+
+// Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// Adicione seu código aqui
+
+names.find((l) => l.length === 5);
+
+// Utilize o find para encontrar a música com id igual a '31031685', caso ela exista
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+
+// Adicione seu código aqui
+
+const idValues = [];
+musicas.forEach((obj, i) => {
+  idValues.push(musicas[i].id);
+});
+idValues.find((id) => id === '31031685');
