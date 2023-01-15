94877ca4939982f6a6dc05bbab6d6be9026e32fb
rnogara <r.nogara.dev@gmail.com>
2023-01-15 10:58:44 -0300

    tentando fazer os testes

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-04-aync-await-e-testes-assincronos/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-04-aync-await-e-testes-assincronos/src/main.js
@@ -1,10 +1,47 @@
-import '../styles.css';
 import Swal from 'sweetalert2';
+import '../styles.css';
 
 const searchBtn = document.querySelector('#search-btn');
 const resultWrapper = document.querySelector('#result-wrapper');
 const cepSize = 8;
 let cepNoHifen = '';
+
+async function btnHandler() {
+  let searchedCep = document.querySelector('#inputCep').value;
+  if (searchedCep === '') {
+    throw new Error('Você precisa passar um CEP');
+  }
+  if (searchedCep.includes('-')) {
+    cepNoHifen = searchedCep.replace('-', '');
+    searchedCep = cepNoHifen;
+  }
+  if (searchedCep.length !== cepSize) {
+    return Swal.fire({
+      icon: 'error',
+      text: 'Cep inválido!',
+    });
+  }
+  const CEP_API = `https://viacep.com.br/ws/${searchedCep}/json/`;
+  const response = await fetch(CEP_API);
+  const data = await response.json();
+  const keys = Object.keys(data);
+  keys.forEach((element) => {
+    if (data[element] !== '') {
+      const result = document.createElement('p');
+      resultWrapper.appendChild(result);
+      result.innerHTML = `<strong>${element}</strong>: ${data[element]}`;
+    }
+  });
+  if (document.querySelector('p').innerHTML === '<strong>erro</strong>: true') {
+    while (resultWrapper.hasChildNodes()) {
+      resultWrapper.removeChild(resultWrapper.lastChild);
+    }
+    Swal.fire({
+      icon: 'error',
+      text: 'Cep inválido!',
+    });
+  }
+}
 
 searchBtn.addEventListener('click', async (e) => {
   e.preventDefault();
@@ -12,38 +49,10 @@ searchBtn.addEventListener('click', async (e) => {
     resultWrapper.removeChild(resultWrapper.lastChild);
   }
   try {
-    let searchedCep = document.querySelector('#inputCep').value;
-    if (searchedCep.includes('-')) {
-      cepNoHifen = searchedCep.replace('-', '');
-      searchedCep = cepNoHifen;
-    }
-    if (searchedCep.length !== cepSize) {
-      return Swal.fire({
-        icon: 'error',
-        text: 'Cep inválido!',
-      });
-    }
-    const CEP_API = `https://viacep.com.br/ws/${searchedCep}/json/`;
-    const response = await fetch(CEP_API);
-    const data = await response.json();
-    const keys = Object.keys(data);
-    keys.forEach((element) => {
-      if (data[element] !== '') {
-        const result = document.createElement('p');
-        resultWrapper.appendChild(result);
-        result.innerHTML = `<strong>${element}</strong>: ${data[element]}`;
-      }
-    });
+    btnHandler();
   } catch (error) {
     Swal.fire(error.message);
   }
-  if (document.querySelector('p').innerHTML === '<strong>erro</strong>: true') {
-    while (resultWrapper.hasChildNodes()) {
-      resultWrapper.removeChild(resultWrapper.lastChild);
-    }
-    return Swal.fire({
-      icon: 'error',
-      text: 'Cep inválido!',
-    });
-  }
 });
+
+export default btnHandler;
