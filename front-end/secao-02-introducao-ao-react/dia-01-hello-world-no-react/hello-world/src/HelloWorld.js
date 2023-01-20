bee64f7703297c565d692b580e1b1cb29ace7a46
rnogara <r.nogara.dev@gmail.com>
2023-01-20 10:49:24 -0300

    hello world

---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/hello-world/src/HelloWorld.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class HelloWorld extends React.Component {
+  render () {
+    return (
+      <span>Hello, World!</span>
+    )
+  }
+}
+
+export default HelloWorld
