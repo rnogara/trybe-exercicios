f91e01a0da4f99b19fa20939ae150abc1808637b
rnogara <r.nogara.dev@gmail.com>
2023-01-23 15:59:47 -0300

    exercicio fixacao

---
+++ front-end/secao-02-introducao-ao-react/dia-02-componentes-react/fixacao/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Image from './Image';
+import cat from './cat.jpg'
+
+function App() {
+  return (
+    <>
+      <Image source={cat} alternativeText='Cute cat staring' />
+    </>
+  );
+}
+
+export default App;
