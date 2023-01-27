f60898b1c1916e598df65bbc4c9694108dbe43dd
rnogara <r.nogara.dev@gmail.com>
2023-01-26 21:03:52 -0300

    exercicio fixacao 2

--- front-end/secao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/fixacao/src/components/Forms.js
+++ front-end/secao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/fixacao/src/components/Forms.js
@@ -7,11 +7,15 @@ class Forms extends React.Component {
     name: '',
     age: '',
     feedback: '',
+    check: '',
+    src: '',
   };
 
-  handleChange = (e) => {
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
-      [e.target.name]: e.target.value,
+      [name]: value,
     });
   }
 
@@ -22,8 +26,14 @@ class Forms extends React.Component {
     document.body.style.backgroundColor = e.target.value;
   };
 
+  handlePhoto = (file) => {
+    this.setState({
+      src: URL.createObjectURL(file),
+    });
+  }
+
   render() {
-    const { name, age, feedback } = this.state;
+    const { name, age, feedback, check, src } = this.state;
     return(
       <form>
         <label htmlFor="color">Escolha uma cor: <select id="color" name="color" onChange={ this.handleBgColor }>
@@ -32,9 +42,40 @@ class Forms extends React.Component {
           <option value='bisque' className="bisque">  </option>
           <option value='lightgreen' className="lightgreen">  </option>
           </select></label>
-        <label htmlFor="name">Nome: <input name="name" id="name" type="text" value={ name } onChange={ this.handleChange } /></label>
-        <label htmlFor="age">Idade: <input name="age" id="age" type="age" value={ age } onChange={ this.handleChange } /></label>
-        <textarea placeholder="Escreva seu feeedback" name="feedback" id="feedback" value={ feedback } onChange={ this.handleChange } />
+        <label htmlFor="name">Nome: <input 
+          name="name" 
+          id="name" 
+          type="text" 
+          value={ name } 
+          onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="age">Idade: <input 
+          name="age" 
+          id="age" 
+          type="number" 
+          value={ age } 
+          onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="feedback">Escreva seu Feedback: <br/>
+          <textarea 
+          placeholder="Escreva seu feeedback" 
+          name="feedback" 
+          id="feedback" 
+          value={ feedback } 
+          onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="check">Gostou? <input 
+          name="check" 
+          id="check" 
+          type="checkbox" 
+          value={ check } 
+          onChange={ this.handleChange } />
+        </label>
+        <fieldset>
+          <legend>Foto do avatar</legend>
+          <img src={ src } alt='foto do usuário' />
+          <input type='file' name='photo' accept=".png, .jpg" onChangeCapture={ ({ target }) => this.handlePhoto(target.files[0]) } />
+        </fieldset>
       </form>
     )
   }
