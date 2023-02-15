ff70961522bd9e31a1f20740a9dc3838236adf26
rnogara <r.nogara.dev@gmail.com>
2023-02-15 19:51:04 -0300

    exercicio de fixação

---
+++ front-end/secao-06-testes-automatizados-com-react-testing-library/dia-01-rtl-primeiros-passos/testes-react/src/components/ValidEmail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const verifyEmail = (email) => {
+  const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+  return emailRegex.test(email);
+};
+
+const ValidEmail = (props) => {
+  const { email } = props;
+  return (
+    <div>
+      <h2 data-testid="id-email-user">{`Valor: ${email}`}</h2>
+      { email && <h3 data-test-id="id-email-valid-text">{(verifyEmail(email) ? <span style={{ color: 'green' }}>Email Válido</span> : <span style={{ color: 'red' }}>Email Inválido</span>)}</h3>}
+    </div>
+  );
+};
+
+ValidEmail.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default ValidEmail;
