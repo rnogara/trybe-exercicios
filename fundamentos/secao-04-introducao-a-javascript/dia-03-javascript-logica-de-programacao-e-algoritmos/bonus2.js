ebdc6fb3709c75c211490c65eb1c0f2c89a0f68e
rnogara <r.nogara.dev@gmail.com>
2023-03-26 18:57:49 -0300

    ajustes

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/bonus2.js
+++
@@ -1,8 +0,0 @@
-const n = 5;
-const draw = '*';
-var line = '*';
-
-for (let l = 1; l <= n; l += 1) {
-  console.log(line);
-  line += draw;
-}
--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/p1-2.js
+++
@@ -1,8 +0,0 @@
-const lotteryChecker = (num, betnum) => num === betnum;
-
-const lotteryResult = (betNumber, callback) => {
-  const lotteryNumber = Math.floor((Math.random() * 5) + 1);
-  return callback(lotteryNumber, betNumber) ? 'Parabéns, você ganhou' : 'Tente novamente';
-};
-
-console.log(lotteryResult(2, lotteryChecker));
