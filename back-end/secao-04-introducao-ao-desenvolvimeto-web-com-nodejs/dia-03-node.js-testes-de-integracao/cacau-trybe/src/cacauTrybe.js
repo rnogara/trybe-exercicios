57c0b0eb0b35c49f7704938d4e79c8cca4fdd4c1
rnogara <r.nogara.dev@gmail.com>
2023-05-15 21:44:46 -0300

    exercicios de pratica

--- back-end/secao-04-introducao-ao-desenvolvimeto-web-com-nodejs/dia-03-node.js-testes-de-integracao/cacau-trybe/src/cacauTrybe.js
+++ back-end/secao-04-introducao-ao-desenvolvimeto-web-com-nodejs/dia-03-node.js-testes-de-integracao/cacau-trybe/src/cacauTrybe.js
@@ -27,8 +27,35 @@ const getChocolatesByBrand = async (brandId) => {
     .filter((chocolate) => chocolate.brandId === brandId);
 };
 
+const findChocolateByName = async (query) => {
+  const cacauTrybe = await readCacauTrybeFile();
+  return cacauTrybe.chocolates
+    .filter((chocolate) => chocolate.name.toLowerCase().includes(query.toLowerCase()));
+};
+
+const updateChocolate = async (id, update) => {
+  const cacauTrybe = await readCacauTrybeFile();
+  const chocolateToUpdate = cacauTrybe.chocolates.find(
+    (chocolate) => chocolate.id === id,
+  );
+
+  if (chocolateToUpdate) {
+    cacauTrybe.chocolates = cacauTrybe.chocolates.map((chocolate) => {
+        if (chocolate.id === id) return { ...chocolate, ...update };
+        return chocolate;
+      });
+  
+    await writeCacauTrybeFile(cacauTrybe);
+    return { ...chocolateToUpdate, ...update };
+  }
+
+  return false;
+};
+
 module.exports = {
   getAllChocolates,
   getChocolateById,
   getChocolatesByBrand,
+  findChocolateByName,
+  updateChocolate
 };
