4903db85c6ce6681e873cc7aff5f43f08fdeace3
rnogara <r.nogara.dev@gmail.com>
2023-07-09 19:05:27 -0300

    exercicios de pratica

---
+++ back-end/secao-09-programacao-orientada-a-objetos-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exercicios5e6.ts
@@ -0,0 +1,91 @@
+class Data {
+  private _day: number;
+  private _month: number;
+  private _year: number;
+
+  constructor(
+    day: number,
+    month: number,
+    year: number
+  ) {
+    if (!Data.validateDate(day, month, year)) {
+      this._day = 1;
+      this._month = 1;
+      this._year = 1900;
+    } else {
+      this._day = day;
+      this._month = month;
+      this._year = year;
+    }
+  }
+
+  get day(): number { return this._day };
+  set day(value: number) { this._day = value};
+
+  get month(): number { return this._month };
+  set month(value: number) { this._day = value};
+
+  get year(): number { return this._year };
+  set year(value: number) { this._year = value};
+
+  getMonthName(): string {
+    const months = [
+      'janeiro',
+      'fevereiro',
+      'março',
+      'abril',
+      'maio',
+      'junho',
+      'julho',
+      'agosto',
+      'setembro',
+      'outubro',
+      'novembro',
+      'dezembro',
+    ];
+    return months[this._month -1];
+  }
+
+  isLeapYear(): boolean { return (this._year % 4 === 0)};
+
+  compare(date: Data): number {
+    const currDateStr = `${this._year}-${this._month}-${this._day}`;
+    const dateStr = `${date._year}-${date._month}-${date._day}`;
+    if (new Date(currDateStr) > new Date(dateStr)) return 1;
+    if (new Date(currDateStr) < new Date(dateStr)) return -1;
+    return 0;
+  }
+
+  format(formatting: string): string {
+    const conditions: boolean[] = [
+      (!formatting.match(/a{2,4}/g)),
+      (!formatting.match(/m{2}/g) && !formatting.match(/M{1}/g)),
+      (!formatting.match(/d{2}/g)),
+    ];
+
+    if (conditions.every((cond) => cond)) {
+      throw new Error(`O formato passado é inválido: ${formatting}`);
+    }
+
+    const day = this.day > 9 ? this.day.toString() : `0${this.day.toString()}`;
+    const month = this.month > 9 
+      ? this.month.toString() 
+      : `0${this.month.toString()}`;
+
+    const year = this.year.toString();
+
+    const dateFormatting = formatting
+      .replace('dd', day).replace('mm', month)
+      .replace('M', this.getMonthName()).replace('aaaa', year)
+      .replace('aa', year.substr(-2));
+
+    return dateFormatting;
+  }
+
+  private static validateDate(day: number, month: number, year: number)
+    : boolean {
+    const dateStr = `${year}-${month}-${day}`;
+    if (new Date(dateStr).getDate() !== day) return false;
+    return true;
+  }
+}
