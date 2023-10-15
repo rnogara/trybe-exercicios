931e0b1f5b32b0204b596f04e4911a70e1fd8397
rnogara <r.nogara.dev@gmail.com>
2023-10-15 10:58:50 -0300

    exercicios para fixar

---
+++ python/secao-04-flask/dia-01-ambiente-e-priemira-api/para-fixar/exercicio_2.py
@@ -0,0 +1,4 @@
+# method: "POST",
+# url: "http://127.0.0.1:8000/jokes",
+# json: {"joke": "Por que o espanador não luta caratê? <br> Porque ele luta
+# capoeira"}
---
+++ python/secao-04-flask/dia-01-ambiente-e-priemira-api/para-fixar/exercicio_3.py
@@ -0,0 +1,3 @@
+# method: "PUT",
+# url: "http://127.0.0.1:8000/jokes/coloque_a_id",
+# json: {"joke": "Escreva aqui a sua nova piada Atualizada"}
